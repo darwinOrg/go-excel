a5d16305c3b51e6d91c89e9d5270d469f9461561
chenbiao <149027310@qq.com>
2024-10-24 17:23:19 +0800

    ExportStruct2XlsxByTemplate

--- exporter.go
+++ exporter.go
@@ -14,7 +14,10 @@ import (
 const defaultSheetName = "Sheet1"
 
 var (
-	columnFlags  = []string{"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+	columnFlags = []string{
+		"A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",
+		"AA", "AB", "AC", "AD", "AE", "AF", "AG", "AH", "AI", "AJ", "AK", "AL", "AM", "AN", "AO", "AP", "AQ", "AR", "AS", "AT", "AU", "AV", "AW", "AX", "AY", "AZ",
+	}
 	urlRegex     = regexp.MustCompile(`^((https|http|ftp|rtsp|mms)?://)\S+$`)
 	nameRegex    = regexp.MustCompile(`name\((.*?)\)`)
 	mappingRegex = regexp.MustCompile(`mapping\((.*?)\)`)
